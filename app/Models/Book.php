7d4a8479d46a9447a58fbafb648b56795205a94f
tanabe003 <cca-003@outlook.jp>
2022-10-13 10:39:05 +0900

    migration再プッシュ

---
+++ app/Models/Book.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    //id以外の「別の主キー名」を設定する
+    protected $
+}
