7a8202e654ca8656e5a8746ad8c2be4f02395600
Hiroto-central <111479634+Hiroto-central@users.noreply.github.com>
2022-10-13 11:17:09 +0900

    シード！

--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -14,6 +14,36 @@ class BooksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $param = [
+        'item_name' => '花子の冒険',
+        'item_number' => '100',
+        'item_amount' =>  '1500',
+        'published' => '2022-10-13 12:00:00',
+        ];
+    DB::table('stock_management_app')->insert($param);
+
+    $param = [
+        'item_name' => '花子の冒険2',
+        'item_number' => '100',
+        'item_amount' =>  '3000',
+        'published' => '2022-10-13 12:00:00',
+        ];
+    DB::table('stock_management_app')->insert($param);
+
+    $param = [
+        'item_name' => '花子の冒険3',
+        'item_number' => '100',
+        'item_amount' =>  '6000',
+        'published' => '2022-10-13 12:00:00',
+        ];
+    DB::table('stock_management_app')->insert($param);
+
+    $param = [
+        'item_name' => '花子の冒険3',
+        'item_number' => '100',
+        'item_amount' =>  '12000',
+        'published' => '2022-10-13 12:00:00',
+        ];
+    DB::table('stock_management_app')->insert($param);
     }
 }
