3fd905803f9eab0c8b67896c38587dfd193351bc
{TakaakiOkubo} <{hilight246@gmail.com}>
2022-10-14 09:52:05 +0900

    updateRoute追加

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,34 @@ Route::delete('/book/{book}',function(Book $book){
     return redirect('/');
 });
 
+Route::post('/books/update', function(Request $request){
+    //バリデーション
+    $validator = Validator::make($request->all(),[
+        'id' => 'required',
+        'item_name'=>'required|max:255',
+        'item_name' => 'required|min:3|max:255',
+        'item_number' => 'required|min:1|max:3',
+        'item_amount' => 'required|max:6',
+        'published' => 'required',
+    ]);
+
+    //バリデーションエラー
+    if($validator->fails()){
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    //Eloquentモデル（登録処理）
+    $books = Book::find($request->id);
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save();
+    return redirect('/');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
