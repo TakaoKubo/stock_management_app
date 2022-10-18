b9e3c35c8e6b54ea30b4d9ba0cd1e99dc81af0fa
{TakaakiOkubo} <{hilight246@gmail.com}>
2022-10-18 14:59:59 +0900

    ソート機能追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,11 +61,11 @@
   <h3>商品並び替え</h3>
   <ul>
     <li><a href="?sort=item_name&order=asc">商品名昇順</a></li>
-    <li><a href="list.html#02">商品名降順</a></li>
-    <li><a href="list.html#03">在庫数昇順</a></li>
-    <li><a href="list.html#04">在庫数降順</a></li>
-    <li><a href="list.html#05">出版年月日昇順</a></li>
-    <li><a href="list.html#06">出版年月日降順</a></li>
+    <li><a href="?sort=item_name&order=desc">商品名降順</a></li>
+    <li><a href="?sort=item_number&order=asc">在庫数昇順</a></li>
+    <li><a href="?sort=item_number&order=desc">在庫数降順</a></li>
+    <li><a href="?sort=published&order=asc">出版年月日昇順</a></li>
+    <li><a href="?sort=published&order=desc">出版年月日降順</a></li>
   </ul>
   <!-- ▲▲listページリンク列ここまで▲▲　-->
   <!-- ▲▲ナビゲーションリンクパートここまで▲▲　-->
