653fa30d900a9ae66d03b919cfc6aef629077c75
{TakaakiOkubo} <{hilight246@gmail.com}>
2022-11-04 09:46:53 +0900

    カートデザイン修正完了

--- resources/views/parts/mycart/section01.blade.php
+++ resources/views/parts/mycart/section01.blade.php
@@ -1,49 +1,75 @@
-<div class="container-fluid">
-   <div class="">
-       <div class="mx-auto" style="max-width:1200px">
-           <h1 class="text-center font-weight-bold" style="color:#555555;  font-size:1.2em; padding:24px 0px;">
-           {{ Auth::user()->name }}さんのカートの中身</h1>
-
-           <div class="">
-               <p class="text-center">{{ $message ?? '' }}</p><br>
-               <div class="d-flex flex-row flex-wrap">
-               @if($my_carts->isNotEmpty()) 
-               @foreach($my_carts as $my_cart)
-                    <div class="mycart_box">
-                        {{$my_cart->stock->item_name}} <br>                                
-                        {{ number_format($my_cart->stock->item_amount)}}円 <br>
-                            <img src="/upload/{{$my_cart->stock->item_img}}" alt="" class="incart" >
-                            <br>
-                            
-                            <form action="/cartdelete" method="post">
+<section class="module">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6 col-sm-offset-3">
+                <h1 class="module-title font-alt">Checkout</h1>
+              </div>
+            </div>
+            <hr class="divider-w pt-20">
+            <div class="row">
+              <div class="col-sm-12">
+                <table class="table table-striped table-border checkout-table">
+                  <tbody>
+                  <p class="text-center">{{ $message ?? '' }}</p><br>
+                    <tr>
+                      <th class="hidden-xs">Item</th>
+                      <th>Description</th>
+                      <th class="hidden-xs">Price</th>
+                      <th>Remove</th>
+                    </tr>
+                    @if($my_carts->isNotEmpty()) 
+                    @foreach($my_carts as $my_cart)
+                    <tr>
+                      <td class="hidden-xs"><a href="#"><img src="/upload/{{$my_cart->stock->item_img}}" alt="INF"/></a></td>
+                      <td>
+                        <h5 class="product-title font-alt">{{$my_cart->stock->item_name}}</h5>
+                      </td>
+                      <td class="hidden-xs">
+                        <h5 class="product-title font-alt">{{ number_format($my_cart->stock->item_amount)}}円</h5>
+                      </td>
+                      <td>
+                      <form action="/cartdelete" method="post">
                                 @csrf
                                 <input type="hidden" name="delete" value="delete">
                                <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
                                 <input type="hidden" name="stock_id" value="{{ $my_cart->stock->id }}">
                                 <input type="submit" value="カートから削除する">
                             </form>
-                    </div>
-                @endforeach
-
-                <div class="text-center p-2">
-                       個数：{{$count}}個<br>
-                       <p style="font-size:1.2em; font-weight:bold;">合計金額:{{number_format($sum)}}円</p>  
-                   </div>  
-                   <form action="/checkout" method="POST">
+                      </td>
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <hr class="divider-w">
+            <div class="row mt-70">
+              <div class="col-sm-5 col-sm-offset-7">
+                <div class="shop-Cart-totalbox">
+                  <h4 class="font-alt">Cart Totals</h4>
+                  <table class="table table-striped table-border checkout-table">
+                    <tbody>
+                      <tr>
+                        <th>Quantity :</th>
+                        <td>{{$count}}個</td>
+                      </tr>
+                      <tr class="shop-Cart-totalprice">
+                        <th>Total :</th>
+                        <td>{{number_format($sum)}}円</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <form action="/checkout" method="POST">
                        @csrf
-                       <button type="submit" class="btn btn-danger btn-lg text-center buy-btn" >購入する</button>
+                       <button type="submit" class="btn btn-lg btn-block btn-round btn-d" >Proceed to Checkout</button>
                    </form>
                    @else
                    <p class="text-center">カートはからっぽです。</p>
-               @endif
 
-               <a href="/">商品一覧へ</a>
-              
-              </div>
-           </div>
-       </div>
-   </div>
-</div>
-
-
-<hr class="divider-w">
+                  @endif
+                  
+                </div>
+              </div><a href="/">商品一覧へ</a>
+            </div>
+          </div>
+        </section>
